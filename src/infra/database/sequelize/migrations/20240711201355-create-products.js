941b78f4b3e9f055d706be64f50cc3440a1ce27d
rafaelsgomes <rafaelsgomes.dev@gmail.com>
2024-07-11 17:21:07 -0300

    Feat: create users model sequelize migration

---
+++ src/infra/database/sequelize/migrations/20240711201355-create-products.js
@@ -0,0 +1,45 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('products', {
+      id: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: true,
+      },
+    })
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('products')
+  },
+}
