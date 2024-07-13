7717f0c826948e40482ce9c735f59d40380f0efb
rafaelsgomes <rafaelsgomes.dev@gmail.com>
2024-07-13 17:33:31 -0300

    Feat: adding swagger to the project

--- src/infra/app.ts
+++ src/infra/app.ts
@@ -1,14 +1,28 @@
 import express from 'express'
 import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import path from 'path'
+import YAML from 'yamljs'
 import { userRoutes } from './http/controllers/users/usersRoutes'
 import passport from './auth/jwt.strategy'
 import { productsRoutes } from './http/controllers/products/productsRoutes'
+
 export const app = express()
 
-app.use(passport.initialize())
-
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: '*',
+  }),
+)
+
+const swaggerDocument = YAML.load(
+  path.join(__dirname, './swagger/swagger.yaml'),
+)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
+app.use(passport.initialize())
 
 app.use(userRoutes)
 app.use(productsRoutes)
